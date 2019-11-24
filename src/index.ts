ce33f0d1ab83cdaa6d7749e9e20567567009f5cc
ChekTek <zhoherchak@gmail.com>
2019-11-24 11:15:39 -0500

    setup for better node distribution

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { minLengthTrimmed } from './lib/min-length-trimmed';
+import { maxLengthTrimmed } from './lib/max-length-trimmed';
+
+export class MoreValidators {
+    static minLengthTrimmed = minLengthTrimmed;
+    static maxLengthTrimmed = maxLengthTrimmed;
+} 
--- src/more-validators.ts
+++
@@ -1,7 +0,0 @@
-import { minLengthTrimmed } from './validators/min-length-trimmed';
-import { maxLengthTrimmed } from './validators/max-length-trimmed';
-
-export class MoreValidators {
-    static minLengthTrimmed = minLengthTrimmed;
-    static maxLengthTrimmed = maxLengthTrimmed;
-} 
